a100c84e71dac8ec763c6079050518f30e20dece
Ron Cagle <ron@atlantashirt.com>
2018-08-23 16:03:18 -0400

    Assignment #3 of the prework
    Make an about me webpage using the MDN lessons.

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+document.querySelector('img').onclick = function() {
+    alert('I am not a robot, promise.');
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Ron Cagle is not a robot but ' + myName + ' might be';
+}
+
+if(!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Ron Cagle is not a robot but ' + storedName + ' might be';
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
